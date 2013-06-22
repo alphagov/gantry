b7f07697c2cc6efcb62b21d64009e59b744da258
Nick Stenning <nick@whiteink.com>
2013-06-22 20:42:41 +0100

    command: expose the tags and exclude_tags options

--- gantry/command.py
+++ gantry/command.py
@@ -37,23 +37,41 @@ def deploy(args):
 
 
 @arg('repository')
-@arg('-t', '--tag')
+@arg('-t', '--tags',
+     help='Only list containers with the specified tags '
+          '(supplied as a comma-separated list)')
+@arg('-x', '--exclude-tags',
+     help='Exclude containers with the specified tags '
+          '(supplied as a comma-separated list)')
 @expects_obj
 def containers(args):
     gantry = Gantry(args.docker_url)
-    for c in gantry.containers(args.repository, tag=args.tag):
+    tags = args.tags.split(',') if args.tags else None
+    exclude_tags = args.exclude_tags.split(',') if args.exclude_tags else None
+    for c in gantry.containers(args.repository,
+                               tags=tags,
+                               exclude_tags=exclude_tags):
         print(c['Id'])
 
 
 @arg('repository')
-@arg('-t', '--tag')
+@arg('-t', '--tags',
+     help='Only list containers with the specified tags '
+          '(supplied as a comma-separated list)')
+@arg('-x', '--exclude-tags',
+     help='Exclude containers with the specified tags '
+          '(supplied as a comma-separated list)')
 @arg('-q', '--quiet', default=False)
 @expects_obj
 def ports(args):
     gantry = Gantry(args.docker_url)
+    tags = args.tags.split(',') if args.tags else None
+    exclude_tags = args.exclude_tags.split(',') if args.exclude_tags else None
     if not args.quiet:
         print("%10s %10s" % ("host_port", "guest_port"))
-    for p in gantry.ports(args.repository, tag=args.tag):
+    for p in gantry.ports(args.repository,
+                          tags=tags,
+                          exclude_tags=exclude_tags):
         print("%10d %10d" % (p[0], p[1]))
 
 parser = ArghParser(version=__version__)
